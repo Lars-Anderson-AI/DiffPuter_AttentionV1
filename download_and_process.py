6d5f230eb08a2cfaa884abb5756fdc20b8f7556f
Lars-Anderson-AI <larsanderson732@gmail.com>
2026-05-07 17:57:59 +0700

    Update download_and_process.py

--- download_and_process.py
+++ download_and_process.py
@@ -7,7 +7,7 @@ import zipfile
 import json
 from generate_mask import generate_mask
 
-DATA_DIR = '/kaggle/working/DiffPuter/datasets'
+DATA_DIR = '/kaggle/working/DiffPuterBased/datasets'
 
 
 NAME_URL_DICT_UCI = {
