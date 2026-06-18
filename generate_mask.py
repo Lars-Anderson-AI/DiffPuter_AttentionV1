3bcc8b47d321b0b8d57e868c3c398c21d3d45627
Lars-Anderson-AI <larsanderson732@gmail.com>
2026-06-18 07:11:20 +0700

    Update generate_mask.py

--- generate_mask.py
+++ generate_mask.py
@@ -14,7 +14,7 @@ from pandas.api.types import is_numeric_dtype
 
 import pdb
 
-DATA_DIR = '/kaggle/working/DiffPuterBased/datasets'
+DATA_DIR = '/kaggle/working/DiffPuter_Attention/datasets'
 torch.set_default_dtype(torch.float32)
 
 '''
